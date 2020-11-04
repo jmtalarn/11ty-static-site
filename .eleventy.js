264289bc39ccf5cc074d4960257564d2d8abb14d
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2020-11-04 13:39:29 +0100

    Clean eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -4,26 +4,6 @@ const { PurgeCSS } = require("purgecss");
 module.exports = (config) => {
   config.addPassthroughCopy("src/css");
 
-  //   config.addTransform("purge-and-inline-css", async (content, outputPath) => {
-  //     if (
-  //       process.env.ELEVENTY_ENV !== "production" ||
-  //       !outputPath.endsWith(".html")
-  //     ) {
-  //       return content;
-  //     }
-
-  //     const purgeCSSResults = await new PurgeCSS().purge({
-  //       content: [{ raw: content }],
-  //       css: ["build/css/style.css"],
-  //       keyframes: true,
-  //     });
-
-  //     return content.replace(
-  //       "<!-- INLINE CSS-->",
-  //       "<style>" + purgeCSSResults[0].css + "</style>"
-  //     );
-  //   });
-
   return {
     dir: {
       input: "src",
